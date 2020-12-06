8c8b54cd3118fc0be66cc43fc81898de3977086d
Ruth Motza <ruth.motza@europace.de>
2020-12-06 16:29:39 +0100

    Add game into main method

--- src/main/kotlin/de/ruth/motza/main.kt
+++ src/main/kotlin/de/ruth/motza/main.kt
@@ -1,5 +1,17 @@
 package de.ruth.motza
 
-fun main(args: Array<String>) {
-  println("Hello World!")
+fun main() {
+  println("Welcome to rock-paper-scissors")
+  print("Start playing")
+  repeat(3){
+    Thread.sleep(2000)
+    print(".")
+  }
+  println()
+  val result = Game().play()
+  println("The results are in:")
+  println("Player 1: ${result.winPlayer1} wins")
+  println("Player 2: ${result.winPlayer2} wins")
+  println("Draws: ${result.draws}")
+  println("And the winner is .... ${result.winner}")
 }
